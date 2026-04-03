c1ba8ce32dc0e4b219f7c107c015775aaf5487cf
Favour O <hugohek@gmail.com>
2026-04-03 09:30:52 -0500

    "added lazy load concept"

--- client/src/components/AppRouter.js
+++ client/src/components/AppRouter.js
@@ -1,7 +1,10 @@
+import React, { Suspense } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import LoginPage from '../pages/LoginPage';
-import SignupPage from '../pages/SignupPage';
-import UserPortal from '../pages/UserPortal';
+
+const LoginPage = React.lazy(() => import('../pages/LoginPage'));
+const SignupPage = React.lazy(() => import('../pages/SignupPage'));
+const UserPortal = React.lazy(() => import('../pages/UserPortal'));
+
 import { APP_ROUTES } from '../Auth/authActions';
 import { useAuth } from '../Auth/authHandler';
 
@@ -17,15 +20,17 @@ const GuestRoute = ({ children }) => {
 
 const AppRouter = () => {
   return (
-    <Routes>
-      <Route path={APP_ROUTES.LOGIN} element={<GuestRoute><LoginPage /></GuestRoute>} />
-      <Route path={APP_ROUTES.SIGNUP} element={<GuestRoute><SignupPage /></GuestRoute>} />
-      <Route path={APP_ROUTES.PROFILE} element={<ProtectedRoute><UserPortal /></ProtectedRoute>} />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path={APP_ROUTES.LOGIN} element={<GuestRoute><LoginPage /></GuestRoute>} />
+        <Route path={APP_ROUTES.SIGNUP} element={<GuestRoute><SignupPage /></GuestRoute>} />
+        <Route path={APP_ROUTES.PROFILE} element={<ProtectedRoute><UserPortal /></ProtectedRoute>} />
 
-      {/* Root and 404 Redirect */}
-      <Route path="/" element={<Navigate to={APP_ROUTES.LOGIN} replace />} />
-      <Route path="*" element={<Navigate to={APP_ROUTES.LOGIN} replace />} />
-    </Routes>
+        {/* Root and 404 Redirect */}
+        <Route path="/" element={<Navigate to={APP_ROUTES.LOGIN} replace />} />
+        <Route path="*" element={<Navigate to={APP_ROUTES.LOGIN} replace />} />
+      </Routes>
+    </Suspense>
   );
 };
 
